0d46484a80444c27f99d748c1205c05027844271
Gantavya Bansal <gantavyaoo@gmail.com>
2026-01-30 00:52:16 -0600

    week4 revsion and architecture

---
+++ R/class4.r
@@ -0,0 +1,55 @@
+# # df1 <- data.frame(
+# #     roll_no=c(1, 2, 3),
+# #     name=c('Amit', 'Neha', 'Ravi'),
+# #     marks=c(20, 12, 98)
+# # )
+# # print(df1)
+
+# df1<-data.frame(
+#   roll_no=c(1,2,3),
+#   name=c("Amit","Neha","Ravi"),
+#   marks=c(78,85,90)
+# )
+# print(df1)
+
+# #1
+# natural_numbers <- 1:10
+# print(natural_numbers)
+
+# #2
+# v<-c("AI","ML","Data Science","Big Data")
+# length(v)
+
+# #3
+# v<-c(5,10,15,20,25)
+# print(v[1])
+# print(v[length(v)])
+
+# #5	
+# v<-c(12, 14, 54, 32, 23)
+# v[v>40]
+# length(v[v<50])
+
+# #8
+# v<- c(10, 20, NA, 20, 50)
+# # sum(v, na.rm= TRUE)
+# # sum(v)
+
+#9
+# v<-c(35, 55, 75, 40, 90)
+# ifelse(v>50, 'pass', 'fail')
+
+# v1<-c(math=85, phy=78, chem=92)
+# v1$phy
+
+
+# no<-c(0:20)
+# no[c(no %% 2 == 0)]
+
+# v3<-c(10, 20, 30, 40 ,50)
+# v3[3]<- 100
+# print(v3)
+
+
+names<-c('Mridul', 'Jay', 'Gantavya')
+sort(names)
